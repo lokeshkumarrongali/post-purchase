6f0930acfec0753bbcfb1089825a2f71bf565329
lokesh-love <lokeshkumarrongali@gmail.com>
2026-01-06 12:43:35 +0530

    chivaridhi

--- post-purchase-ui/src/components/AddOnCard.jsx
+++ post-purchase-ui/src/components/AddOnCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function AddOnCard({ title, benefit, reason, onAdd, onSkip, isSelected }) {
+function AddOnCard({ title, benefit, reason, onAdd, onSkip }) {
   return (
     <div style={{ border: '1px solid #ddd', padding: '1rem', marginTop: '1rem', borderRadius: '8px' }}>
       <h4>{title}</h4>
@@ -8,11 +8,7 @@ function AddOnCard({ title, benefit, reason, onAdd, onSkip, isSelected }) {
       <p style={{ fontSize: '0.9em', color: '#555' }}><em>Why: {reason}</em></p>
       
       <div style={{ marginTop: '1rem' }}>
-        {isSelected ? (
-          <span style={{ color: 'green', fontWeight: 'bold', marginRight: '1rem' }}>Added!</span>
-        ) : (
-          <button onClick={onAdd} style={{ marginRight: '1rem' }}>Add</button>
-        )}
+        <button onClick={onAdd} style={{ marginRight: '1rem' }}>Add</button>
         <button onClick={onSkip}>Skip</button>
       </div>
     </div>
